782f377061b4a2d79b873c32c45edc331e404f07
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-29 22:35:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,11 @@ def using_insert
   another_language = "Python"
   new_array = list_of_programming_languages.insert(4,another_language)
 end
+
+def using_uniq
+  it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
+    haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+    new_array = using_uniq(haircuts)
+    expect(new_array).to eq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"])
+  end
+end
