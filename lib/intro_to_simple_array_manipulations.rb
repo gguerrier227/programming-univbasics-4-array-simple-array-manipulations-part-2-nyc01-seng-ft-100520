f87597ad72dbd04b51b743128a3548d04506c849
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-29 22:32:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,11 @@
-def using_concat do
+def using_concat
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   all_my_favs = my_favorite_things.concat(more_favs)
 end
+
+def using_insert
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  new_array = list_of_programming_languages.insert(another_language)
+  end
