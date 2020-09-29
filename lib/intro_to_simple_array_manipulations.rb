a2eda3bbfcf3c57192ae2a380df7380b8a0cba1e
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-29 22:47:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,7 @@ end
 describe "using_delete_at" do 
   it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
     famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-    deleted_robot = famous_robots.delete.at
+    deleted_robot = famous_robots.delete_at
     expect(deleted_robot).to eq("Robocop")
   end
 end
