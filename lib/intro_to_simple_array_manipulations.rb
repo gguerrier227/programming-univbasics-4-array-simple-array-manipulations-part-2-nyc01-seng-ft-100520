48e05c2eba39170124fd26c86345b62625a106cc
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-29 22:29:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,5 @@
+def using_concat do
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  all_my_favs = using_concat(@my_favorite_things, more_favs)
+end
